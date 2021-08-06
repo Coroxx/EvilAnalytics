3505037540af580541a697939556ff3b6909361b
Corox <45209113+Coroxx@users.noreply.github.com>
2021-08-06 10:07:15 +0200

    🚧 WIP 🚧
    - Fixing some bugs and added some new features

--- AnalyticsController.php
+++ AnalyticsController.php
@@ -4,18 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Call;
+use Carbon\Carbon;
 
 class AnalyticsController extends Controller
 {
+    private function forarray($requests, $array, $string)
+    {
+        foreach ($requests as $object) {
+            array_push($array, $object->$string);
+        }
+
+        return $array;
+    }
+
     public function index()
     {
         // Collecting unique user count
 
-        $unique_users = Call::whereDate('created_at', '>=', now()->subDays(1))->distinct('session_id')->count();
+        $unique_users_today = Call::whereDate('created_at', Carbon::today())->distinct('session_id')->count();
+        $unique_users_week = Call::whereDate('created_at', '>=', now()->subWeek(1))->distinct('session_id')->count();
 
         // Get requests by date 
 
-        $today_requests = Call::whereDate('created_at', '>=', now()->subDays(1))->count();
+        $today_requests = Call::whereDate('created_at', Carbon::today())->count();
         $week_requests = Call::whereDate('created_at', '>=', now()->subDays(7))->get();
         $month_requests = Call::whereDate('created_at', '>=', now()->subMonth(1))->get();
 
@@ -23,37 +34,36 @@ class AnalyticsController extends Controller
         // Collecting the most present country 
 
         $week_countries = [];
-        foreach ($week_requests as $country) {
-            array_push($week_countries, $country->country);
-        }
-        $most_present_country = collect($week_countries)->countBy()->sortDesc()->keys()->first();
+
+        $week_countries = $this->forarray($week_requests, $week_countries, 'country');
+
+        $most_present_country = collect($week_countries)->filter()->countBy()->sortDesc()->keys();
 
 
         // Collecting the most present device 
 
         $week_devices = [];
-        foreach ($week_requests as $device) {
-            array_push($week_devices, ucfirst($device->device));
-        }
-        $most_present_device = collect($week_devices)->countBy()->sortDesc()->keys()->first();
+
+
+        $week_devices = $this->forarray($week_requests, $week_devices, 'device');
+        $most_present_device = collect($week_devices)->filter()->countBy()->sortDesc()->keys();
 
         // Collecting the
 
         $month_routes = [];
-        foreach ($month_requests as $route) {
-            array_push($month_routes, strtolower($route->route));
-        }
+
+        $month_routes = $this->forarray($month_requests, $month_routes, 'route');
+        $month_routes = collect($month_routes)->filter()->countBy()->sortDesc()->keys();
 
         // Most present route
 
-        $most_visited_route = collect($month_routes)->countBy()->sortDesc()->keys()->first();
+        $most_visited_route = $month_routes->first();
 
         // Least visited url
 
-        $no_visited_route = collect($month_routes)->countBy()->sortDesc()->keys()->last();
+        $no_visited_route = $month_routes->last();
 
 
-
-        return view('analytics.index', compact('unique_users', 'today_requests', 'most_present_device', 'most_present_country', 'most_visited_route', 'no_visited_route'));
+        return view('analytics.index', compact('unique_users_today', 'today_requests', 'unique_users_week', 'month_routes', 'most_present_device', 'most_present_country', 'week_requests', 'most_visited_route', 'no_visited_route'));
     }
 }
