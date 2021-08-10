1fbe0eb32250688a6362f98372edf37b02cf0dea
Corox <45209113+Coroxx@users.noreply.github.com>
2021-08-10 10:09:52 +0200

    🚚 Excluding analytics routes from the AnalyticsMiddleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-use coroxx\evilanalytics\Http\Middleware\AnalyticMiddleware;
-
 Route::group(['namespace' => 'coroxx\evilanalytics\Http\Controllers'], function () {
-    Route::middleware([AnalyticMiddleware::class])->group(function () {
         Route::get('analytics/{lang}', 'AnalyticsController@index')->name('analytics');
         Route::get('analytics', 'AnalyticsController@indexLang')->name('analytics.index');
-    });
 });
-
-
-
-// Route::get('evilanalytics', function () {
-//     return 'evilanalytics';
-// });
